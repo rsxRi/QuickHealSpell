f76254585afcf1323419fbd3769b0fe0b9897f25
extract <decodeish@gmail.com>
2020-06-07 21:47:14 +0200

    Add template files

---
+++ Level_MasterQuickHealSpell.cs
@@ -0,0 +1,23 @@
+﻿using BS;
+using System;
+using UnityEngine;
+
+namespace QuickHealSpell
+{
+    public class Level_MasterQuickHealSpell : LevelModule
+    {
+        public override void OnLevelLoaded(LevelDefinition levelDefinition)
+        {
+            initialized = true;
+        }
+        public override void Update(LevelDefinition levelDefinition)
+        {
+            if (!initialized) return;
+            // Add code here.
+        }
+        public override void OnLevelUnloaded(LevelDefinition levelDefinition)
+        {
+            initialized = false;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QuickHealSpell")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("extract")]
+[assembly: AssemblyProduct("QuickHealSpell.Properties")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35e0329e-8a7e-4fe4-b71a-93a4474d4dd8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
