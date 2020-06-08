50b321ae0fb17e18ff777e5e0747cdf0f7d6843f
extract <decodeish@gmail.com>
2020-06-08 21:04:25 +0200

    Fix crush type

--- src/QuickHealSpell/Spell_QuickHealSpell.cs
+++ src/QuickHealSpell/Spell_QuickHealSpell.cs
@@ -12,6 +12,15 @@ namespace QuickHealSpell
 		public float healChargePercent = 50f;
 		public float exponentGrowth = 1.3f;
 		public float gripThreshold = 0.7f; // [0, 1]
+		public string spellHealType = "crush"; // [crush, smash, constant]
+		private static SpellHealType spellHealTypeInternal = SpellHealType.Crush;
+
+		enum SpellHealType
+        {
+			Crush,
+			Smash,
+			Constant
+        }
 
 		public new SpellCastProjectile Clone()
 		{
@@ -22,7 +31,22 @@ namespace QuickHealSpell
 		{
 			base.OnCatalogRefresh();
 			imbueEnabled = false;
+			VerifyType();
 		}
+
+        private void VerifyType()
+        {
+			foreach(SpellHealType healType in Enum.GetValues(typeof(SpellHealType)))
+            {
+				if (healType.ToString().ToLower().Contains(spellHealType.ToLower()))
+                {
+					spellHealTypeInternal = healType;
+					Debug.Log("QuickHealSpell set to " + healType.ToString());
+					return;
+                }
+            }
+			Debug.LogError("No valid type has been assigned to QuickHealSpell.");
+        }
 
         public override void UpdateCaster()
         {
@@ -33,7 +57,8 @@ namespace QuickHealSpell
 			foreach (Side hand in Enum.GetValues(typeof(Side))) 
 			{
 				if (PlayerControl.GetHand(hand).gripPressed && 
-					PlayerControl.GetHand(hand).GetAverageCurl() > gripThreshold)
+					PlayerControl.GetHand(hand).GetAverageCurlNoThumb() > gripThreshold &&
+					spellHealTypeInternal == SpellHealType.Crush)
                 {
 					HealSelf(true);
                 }
@@ -44,15 +69,20 @@ namespace QuickHealSpell
         public override void Fire(bool active)
         {
             base.Fire(active);
-        }
+			if (active) return;
+			base.spellCaster.isFiring = false;
+			base.spellCaster.grabbedFire = false;
+			base.spellCaster.telekinesis.TryRelease(false);
+		}
 
 		private void HealSelf(bool active)
 		{
 			if (!active || this.currentCharge < (healChargePercent / 100f)) return;
 
+			Debug.Log("Healing player " + Mathf.Pow(this.currentCharge, exponentGrowth) * baseHeal);
 			Fire(false);
 			Creature.player.health.Heal(Mathf.Pow(this.currentCharge, exponentGrowth) * baseHeal, Creature.player);
-			
+			currentCharge = 0;
 		}	
     }
 }
